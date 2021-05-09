9b9aa4ec55fb3329d90ac664d0ef15fb5e544b51
変態の先生 <teehc19@student.cbhs.school.nz>
2021-05-10 00:21:36 +1200

    Navbar Positioning Application to All Pages
    Repositioned navbar button in title. Resized content

--- Japanese Website/japanese.js
+++ Japanese Website/japanese.js
@@ -2,13 +2,11 @@ function toggleNav() {
  if (document.getElementById("mySidebar").style.width < "10px"){
      document.getElementById("mySidebar").style.width = "15vw";
      document.getElementById("main1").style.left = "7.5vw";
-     document.getElementById("openbtn").style.left = "7.5vw";
      document.getElementById("main2").style.left = "7.5vw";
 }
 
  else { document.getElementById("mySidebar").style.width = "0px";
        document.getElementById("main1").style.left = "0px";
        document.getElementById("main2").style.left = "0px";
-       document.getElementById("openbtn").style.left = "0px";
  }
 }
