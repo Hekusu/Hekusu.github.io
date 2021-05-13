44e62f4e0f4c7f20e39152116366fa8f13ececbc
変態の先生 <teehc19@student.cbhs.school.nz>
2021-05-14 02:45:47 +1200

    Content for Almost All Pages + Added extra js

--- japanese.js
+++ japanese.js
@@ -3,10 +3,12 @@ function toggleNav() {
      document.getElementById("mySidebar").style.width = "15vw";
      document.getElementById("main1").style.left = "7.5vw";
      document.getElementById("main2").style.left = "7.5vw";
+     document.getElementById("main3").style.left = "7.5vw";
 }
 
  else { document.getElementById("mySidebar").style.width = "0px";
        document.getElementById("main1").style.left = "0px";
        document.getElementById("main2").style.left = "0px";
+       document.getElementById("main3").style.left = "0px";
  }
 }
